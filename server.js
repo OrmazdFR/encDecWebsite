4f830c6d6b31cd4ffa1b7ed038304114fb6be321
Ormazd <pbisset@protonmail.com>
2022-01-07 17:02:45 +0100

    Page + field created
    checkpoint

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
-
 const express = require('express');
+var SHA256 = require("crypto-js/sha256");
 
 // Constants
 const PORT = 8080;
@@ -9,8 +9,20 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	const mainHTML = 'Select the hash algorithm:<br><a href="sha256">SHA256</a>'
+	res.send(mainHTML);
+});
+
+app.get('/sha256', (req, res) => {
+	const sha256HTML = 'Your text to hash: <input name="toHash" type="text"></input><button onClick="hashSHA256">Hash</button><br>Hash result:<span id="hashResult">empty</span>'
+	res.send(sha256HTML);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+function hashSHA256() {
+	hrSpan = document.getElementById("hashResult").innerHTML;
+	hrSpan.innerHTML = "detected but not ready yet (to implement)";
+}
